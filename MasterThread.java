2d02edf3e4d6886a2a8580621b8e3ce5a18842cf
Vijaya Kaushika Akella <vkakella@Vijayas-MacBook-Pro.local>
2021-03-21 15:12:54 -0500

    added master thread class

--- MasterThread.java
+++ MasterThread.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//global variables
+// MIS_set = {}
+// awake = [T, T, T, T, T]
+// processStatus = [winner, loser, undecided]
+
 public class MasterThread extends Thread {
     List<Integer> processStatus = new ArrayList<>();
-    
+
 }
